837063e95f22588fee670b1866cc437c173f39b6
Dilupa Sampath <madsampath@Virtusa.com>
2018-08-10 20:19:56 +0530

    asdasdasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,NO_ERRORS_SCHEMA  } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import {RouterModule,Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SidenavBarComponent } from './components/sidenav-bar/sidenav-bar.component';
+import { PostItemComponent } from './components/post-item/post-item.component';
+const appRouts:Routes=[
+  
+  {
+    path:'postItem',component:PostItemComponent
+  }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SidenavBarComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(
+      appRouts
+    ),
     MDBBootstrapModule.forRoot()
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
